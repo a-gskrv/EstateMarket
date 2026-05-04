509609a0b5bf6dae2a235c7b47f5d7fffacee7c3
a-gskrv <agskrv@gmail.com>
2026-05-04 19:40:20 +0200

    Create base Django apps and project structure. Closes #1

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    "users",
+    "listings",
+    "bookings",
+    "reviews",
 ]
 
 MIDDLEWARE = [

f0d50c2be3c99234e58b3e1bc593b27ffd44ba29
a-gskrv <agskrv@gmail.com>
2026-05-05 03:11:02 +0200

    Add review model and run migrations. Refs #2

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,55 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from bookings.models import Booking
+from users.models import User
+
+
+class Review(models.Model):
+    booking = models.ForeignKey(
+        Booking,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    rating = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ],
+    )
+
+    review_text = models.TextField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
+    )
+
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "reviews_review"
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+
+    def __str__(self):
+        return f"{self.user}: {self.rating} -> ({self.booking.listing.title})"
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_active = False
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        return self.save(update_fields=['is_active', 'is_deleted', 'deleted_at'])
